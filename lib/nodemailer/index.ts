3ab9cf78cc9f17a94b4031dfe61c2aa06ad41373
Gary Marc Killen <killengary37@gmail.com>
2025-10-22 09:38:37 -0700

    Implement Daily news summary

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -33,7 +33,7 @@ export const sendNewsSummaryEmail = async (
         .replace('{{newsContent}}', newsContent);
 
     const mailOptions = {
-        from: `"Signalist News" <signalist@jsmastery.pro>`,
+        from: `"EquiTrade News" <EquiTrade@gmail.com>`,
         to: email,
         subject: `📈 Market News Summary Today - ${date}`,
         text: `Today's market news summary from EquiTrade`,
